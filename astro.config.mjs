ee0fcb50ae2bd469dbe76a3dd58d2efecbe3033e
abizmo <8437730+abizmo@users.noreply.github.com>
2025-02-08 18:18:14 +0000

    Add SVG icons and update TypeScript configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import vercel from '@astrojs/vercel';
-import react from '@astrojs/react';
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [react()]
+  integrations: [react()],
+  experimental: {
+    svg: true,
+  },
 });
