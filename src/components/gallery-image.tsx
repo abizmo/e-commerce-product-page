a3d11fa9ff63d9807e234223d55f068afadd7fbc
abizmo <8437730+abizmo@users.noreply.github.com>
2025-03-16 09:08:23 +0000

    feat: update lightbox integration in gallery image component and enhance main page layout

--- src/components/gallery-image.tsx
+++ src/components/gallery-image.tsx
@@ -1,6 +1,6 @@
 import { useStore } from "@nanostores/react";
 import { gallery } from "@stores/gallery";
-import { open } from "@stores/lightbox";
+import { openLightbox } from "@stores/lightbox";
 
 function GalleryImage() {
   const { selectedImage, images } = useStore(gallery);
@@ -9,7 +9,7 @@ function GalleryImage() {
   const { src, alt } = images[selectedImage];
 
   return (
-    <button onClick={open} style={{ padding: 0 }}>
+    <button onClick={openLightbox} style={{ padding: 0 }}>
       <img src={src} alt={alt} />
     </button>
   );
