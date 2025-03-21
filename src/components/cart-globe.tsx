dbdebe0d8aec4cc608fbe16939b2596fbe006d38
abizmo <8437730+abizmo@users.noreply.github.com>
2025-03-21 15:05:47 +0000

    feat: Add CartGlobe component to display total items in cart

---
+++ src/components/cart-globe.tsx
@@ -0,0 +1,25 @@
+import { useStore } from "@nanostores/react";
+import { cart as cartStore } from "@stores/cart";
+import style from "@styles/cart-globe.module.css";
+
+interface CartGlobeProps {
+  className: string;
+}
+
+function CartGlobe({ className, ...rest }: CartGlobeProps) {
+  const cart = useStore(cartStore);
+  if (cart.length === 0) return null;
+  const totalItems = cart.reduce(
+    (total, product) => total + product.quantity,
+    0
+  );
+
+  console.log(cart);
+  return (
+    <span className={[style.cartGlobe, className].join(" ")} {...rest}>
+      {totalItems}
+    </span>
+  );
+}
+
+export default CartGlobe;
